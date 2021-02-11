06d384e165495cb3d9b886336ca0118b0a71d4b3
bc-chaz <chaz.lowe@bigcommerce.com>
2021-02-11 15:25:33 -0800

    feat(common): step 4 design refinements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Panel, Text } from '@bigcommerce/big-design';
+import { Box, Flex, H2, Link, Panel, Text } from '@bigcommerce/big-design';
 import Header from '../components/header';
 import { useProducts } from '../lib/hooks';
 
@@ -6,25 +6,28 @@ const Index = () => {
     const { summary } = useProducts();
 
     return (
-        <Panel margin="xxLarge">
-            <Header title="Homepage" />
-            {summary &&
-                <Flex>
-                    <Box marginRight="xLarge">
-                        <Text>Inventory Count</Text>
-                        <Text>{summary.inventory_count}</Text>
-                    </Box>
-                    <Box marginRight="xLarge">
-                        <Text>Variant Count</Text>
-                        <Text>{summary.variant_count}</Text>
-                    </Box>
-                    <Box>
-                        <Text>Primary Category</Text>
-                        <Text>{summary.primary_category_name}</Text>
-                    </Box>
-                </Flex>
-            }
-        </Panel>
+        <>
+            <Header />
+            <Panel marginHorizontal="xxLarge">
+                <H2>Homepage</H2>
+                {summary &&
+                    <Flex>
+                        <Box marginRight="xLarge">
+                            <Text>Inventory Count</Text>
+                            <Text>{summary.inventory_count}</Text>
+                        </Box>
+                        <Box marginRight="xLarge">
+                            <Text>Variant Count</Text>
+                            <Text>{summary.variant_count}</Text>
+                        </Box>
+                        <Box>
+                            <Text>Primary Category</Text>
+                            <Text>{summary.primary_category_name}</Text>
+                        </Box>
+                    </Flex>
+                }
+            </Panel>
+        </>
     );
 };
 
