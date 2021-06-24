a2e4d86064cc55427779e49a6c95d1a3e8c70a55
bc-chaz <chaz.lowe@bigcommerce.com>
2021-06-24 15:51:40 -0700

    feat(common): LFG-177 corrects IDOR

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -2,16 +2,16 @@ import useSWR from 'swr';
 import { useSession } from '../context/session';
 import { ListItem } from '../types';
 
-function fetcher(url: string, storeHash: string) {
-    return fetch(`${url}?context=${storeHash}`).then(res => res.json());
+function fetcher(url: string, encodedContext: string) {
+    return fetch(`${url}?context=${encodedContext}`).then(res => res.json());
 }
 
 // Reusable SWR hooks
 // https://swr.vercel.app/
 export function useProducts() {
-    const storeHash = useSession()?.storeHash;
+    const encodedContext = useSession()?.context;
     // Request is deduped and cached; Can be shared across components
-    const { data, error } = useSWR(storeHash ? ['/api/products', storeHash] : null, fetcher);
+    const { data, error } = useSWR(encodedContext ? ['/api/products', encodedContext] : null, fetcher);
 
     return {
         summary: data,
@@ -21,9 +21,9 @@ export function useProducts() {
 }
 
 export function useProductList() {
-    const storeHash = useSession()?.storeHash;
+    const encodedContext = useSession()?.context;
     // Use an array to send multiple arguments to fetcher
-    const { data, error, mutate: mutateList } = useSWR(storeHash ? ['/api/products/list', storeHash] : null, fetcher);
+    const { data, error, mutate: mutateList } = useSWR(encodedContext ? ['/api/products/list', encodedContext] : null, fetcher);
 
     return {
         list: data,
@@ -34,10 +34,10 @@ export function useProductList() {
 }
 
 export function useProductInfo(pid: number, list: ListItem[]) {
-    const storeHash = useSession()?.storeHash;
+    const encodedContext = useSession()?.context;
     const product = list.find(item => item.id === pid);
     // Conditionally fetch product if it doesn't exist in the list (e.g. deep linking)
-    const { data, error } = useSWR(!product && storeHash ? [`/api/products/${pid}`, storeHash] : null, fetcher);
+    const { data, error } = useSWR(!product && encodedContext ? [`/api/products/${pid}`, encodedContext] : null, fetcher);
 
     return {
         product: product ?? data,
