ebdc99297a947f942bb67236d9560c1df2149313
Chancellor Clark <chancellor.clark@bigcommerce.com>
2021-11-05 11:22:02 -0500

    feat(common): use jwt url value for load redirect

--- pages/api/load.ts
+++ pages/api/load.ts
@@ -1,6 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { encodePayload, getBCVerify, setSession } from '../../lib/auth';
 
+const buildRedirectUrl = (url: string, encodedContext: string) => {
+    const [path, query = ''] = url.split('?');
+    const queryParams = new URLSearchParams(`context=${encodedContext}&${query}`);
+
+    return `${path}?${queryParams}`;
+}
+
 export default async function load(req: NextApiRequest, res: NextApiResponse) {
     try {
         // Verify when app loaded (launch)
@@ -8,7 +15,7 @@ export default async function load(req: NextApiRequest, res: NextApiResponse) {
         const encodedContext = encodePayload(session); // Signed JWT to validate/ prevent tampering
 
         await setSession(session);
-        res.redirect(302, `/?context=${encodedContext}`);
+        res.redirect(302, buildRedirectUrl(session.url, encodedContext));
     } catch (error) {
         const { message, response } = error;
         res.status(response?.status || 500).json({ message });
