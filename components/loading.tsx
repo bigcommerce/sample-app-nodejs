5f7b07d3ea002a69f5be2f0dcc16e69d72c52ded
Monday_David_Sarka <david_md@hamari.agency>
2022-04-12 15:01:00 +0100

    bigcommerce init update

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,7 +2,7 @@ import { Flex, H3, Panel, ProgressCircle } from '@bigcommerce/big-design';
 
 const Loading = () => (
     <Panel>
-        <H3>Loading...</H3>
+        <H3>Hamari Loads....</H3>
         <Flex justifyContent="center" alignItems="center">
             <ProgressCircle size="large" />
         </Flex>
