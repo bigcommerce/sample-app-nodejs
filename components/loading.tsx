796f2e6bf87acd26831669df854e9cb347aef139
Monday_David_Sarka <david_md@hamari.agency>
2022-04-13 19:55:07 +0100

    Updated typescript to latest version

--- components/loading.tsx
+++ components/loading.tsx
@@ -2,7 +2,7 @@ import { Flex, H3, Panel, ProgressCircle } from '@bigcommerce/big-design';
 
 const Loading = () => (
     <Panel>
-        <H3>Hamari Loads....</H3>
+        <H3>Loads...</H3>
         <Flex justifyContent="center" alignItems="center">
             <ProgressCircle size="large" />
         </Flex>
