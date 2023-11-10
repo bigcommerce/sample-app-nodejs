61fee6357d4d886cfd1077f52d9fecca7b989af6
DavidZento <david.simba@zento.fr>
2023-11-10 16:49:00 +0100

    custom packing light

--- components/PackingSlip.tsx
+++ components/PackingSlip.tsx
@@ -29,65 +29,57 @@ const PackingSlip = ({ order }: PackingSlipProps) => {
             <div className="PackingSlip">
                 <div style={{fontSize: "2.5rem"}} className="PackingSlipTitle">
                     %%GLOBAL_StoreName%% %%GLOBAL_PackingSlipTitle%%
-                </div>
+                </div><br/><br/>
 
-                <div style={{fontSize: "2.5rem"}} className="StoreAddress">
+                <div style={{fontSize: "2rem"}} className="StoreAddress">
                     %%GLOBAL_StoreAddressFormatted%%
-                </div>
+                </div><br/><br/>
 
-                <div style={{fontSize: "2.5rem"}} className="AddressRow">
+                <div style={{fontSize: "2rem"}} className="AddressRow">
                     <div className="BillingAddress">
-                        <div className="PackingSlipHeading">%%LNG_BillTo%%</div>
+                        <div style={{fontSize: "2rem"}} className="PackingSlipHeading">%%LNG_BillTo%%</div>
                         %%GLOBAL_BillingAddress%%
                         <div style={{/**"%%GLOBAL_HideBillingPhone%%" */}}>
                             %%LNG_Phone%%: %%GLOBAL_BillingPhone%%
                         </div>
                     </div>
                     <div className="ShippingAddress">
-                        <div className="PackingSlipHeading">%%LNG_ShipTo%%</div>
+                        <div style={{fontSize: "2rem"}} className="PackingSlipHeading">%%LNG_ShipTo%%</div>
                         %%GLOBAL_ShippingAddress%%
                         <div style={{/**"%%GLOBAL_HideShippingPhone%%" */}}>
                             %%LNG_Phone%%: %%GLOBAL_ShippingPhone%%
                         </div>
                     </div>
-                </div>
+                </div><br/><br/>
 
-                <div style={{fontSize: "2.5rem"}} className="PackingSlipDetails">
+                <div style={{fontSize: "2rem"}} className="PackingSlipDetails">
                     <div className="PackingSlipDetailsLeft">
                         <div className="DetailRow">
                             <div className="Label">%%LNG_Order%%:</div>
                             <div className="Value">#%%GLOBAL_OrderId%%</div>
                         </div>
-                        <div className="DetailRow">
-                            <div className="Label">%%LNG_ShipmentOrderDate%%:</div>
-                            <div className="Value">%%GLOBAL_OrderDate%%</div>
+                        <div style={{display: "flex"}}>
+                            <div style={{fontWeight: 'bold'}}>%%LNG_ShipmentOrderDate%%:</div>
+                            <div style={{marginLeft: "5%"}}>%%GLOBAL_OrderDate%%</div>
                         </div>
                     </div>
                     <div className="PackingSlipDetailsRight">
-                        <div className="DetailRow" style={{/**"%%GLOBAL_HideShippingMethod%%" */}}>
-                            <div className="Label">%%LNG_ShippingMethod%%:</div>
-                            <div className="Value">%%GLOBAL_ShippingMethod%%</div>
-                        </div>
-                        <div className="DetailRow" style={{/**"%%GLOBAL_HideShippingDate%%" */}}>
-                            <div className="Label">%%LNG_DateShipped%%:</div>
-                            <div className="Value">%%GLOBAL_DateShipped%%</div>
-                        </div>
-                        <div className="DetailRow" style={{/**"%%GLOBAL_HideTrackingNo%%" */}}>
-                            <div className="Label">%%LNG_TrackingNumber%%:</div>
-                            <div className="Value">%%GLOBAL_TrackingNo%%</div>
+                        <div style={{display: "flex"}}>
+                            <div style={{fontWeight: 'bold'}}>%%LNG_ShippingMethod%%:</div>
+                            <div style={{marginLeft: "5%"}}>%%GLOBAL_ShippingMethod%%</div>
                         </div>
                     </div>
-                </div>
+                </div><br/><br/>
 
                 <div className="PackingSlipComments" style={{/**"%%GLOBAL_HideComments%%" */}}>
-                    <div style={{fontSize: "2.5rem"}} className="PackingSlipHeading">%%LNG_Comments%%</div>
-                    <blockquote style={{fontSize: "2.5rem"}}>
+                    <div style={{fontSize: "2rem", marginTop: "3%"}} className="PackingSlipHeading">%%LNG_Comments%%</div>
+                    <blockquote style={{fontSize: "2rem"}}>
                         %%GLOBAL_Comments%%
                     </blockquote>
-                </div>
+                </div><br /><br/>
 
                 <div className="PackingSlipItems">
-                    <div className="PackingSlipHeading">%%LNG_ShippedItems%%</div>
+                    <div style={{fontSize: "2rem", marginTop: "3%"}} className="PackingSlipHeading">%%LNG_ShippedItems%%</div><br /><br/>
                     <table style={{
                         width: '100vw',
                         height: 'fit-content'
@@ -95,19 +87,19 @@ const PackingSlip = ({ order }: PackingSlipProps) => {
                         <thead style={{border: '1px solid black', borderBottom: "none"}}>
                             <tr style={{paddingTop: "30px", paddingBottom: "30px"}}>
                                 <th style={{width: "20%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center"}}></th>
-                                <th style={{width: "30%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2.5rem"}}>%%LNG_Code%%</th>
-                                <th style={{width: "10%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2.5rem"}}>%%LNG_Quantity%%</th>
-                                <th style={{width: "30%", paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2.5rem"}}>%%LNG_ProdBin%%</th>
+                                <th style={{width: "30%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2rem"}}>%%LNG_Code%%</th>
+                                <th style={{width: "10%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2rem"}}>%%LNG_Quantity%%</th>
+                                <th style={{width: "30%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2rem"}}>%%LNG_ProdBin%%</th>
                                 <th style={{width: "10%"}}></th>
                             </tr>
                         </thead>
                         <tbody style={{border: '1px solid black'}}>
                             {products && products.map((product: any, index: number) => (
                                 <tr style={{borderBottom: "1px solid black", borderTop: index === (products - 1) && "1px solid black"}}>
-                                    <td style={{textAlign: "center", borderRight: "1px solid black", borderLeft: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}><img src={product.data.image_url} style={{width: "90px", height: "105px"}} /></td>
+                                    <td style={{width: "20%", textAlign: "center", borderRight: "1px solid black", borderLeft: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}><img src={product.data.image_url} style={{width: "168px", height: "196px"}} /></td>
                                     <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}>{product.data.sku}</td>
                                     <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}>12</td>
-                                    <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}>hfxfgx5641654569415</td>
+                                    <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}>jhvjlhcvlmzV54645</td>
                                     <td></td>
                                 </tr>
                             ))}
