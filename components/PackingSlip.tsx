942bf9f119f3e96ad2994e9fd507bad2a5d7c3ab
DavidZento <david.simba@zento.fr>
2023-11-10 16:50:48 +0100

    full custom packing slip

--- components/PackingSlip.tsx
+++ components/PackingSlip.tsx
@@ -31,13 +31,13 @@ const PackingSlip = ({ order }: PackingSlipProps) => {
                 Zag Bijoux Bordereau de préparation {orderId}
                 </div><br/><br/>
 
-                <div style={{fontSize: "2rem"}} className="StoreAddress">
+                <div style={{fontSize: "1.5rem"}} className="StoreAddress">
                     {storeAdress}
                 </div><br /><br/>
 
-                <div style={{fontSize: "2rem"}} className="AddressRow">
+                <div style={{fontSize: "1.5rem"}} className="AddressRow">
                     <div className="BillingAddress">
-                        <div style={{fontSize: "2rem"}} className="PackingSlipHeading">Billing Details</div>
+                        <div style={{fontSize: "1.5rem"}} className="PackingSlipHeading">Billing Details</div>
                             <div>
                                 <div>{billingAdress.first_name} {billingAdress.last_name}</div>
                                 <div>{billingAdress.company}</div>
@@ -48,7 +48,7 @@ const PackingSlip = ({ order }: PackingSlipProps) => {
                             </div>
                     </div>
                     <div className="ShippingAddress">
-                        <div style={{fontSize: "2rem"}} className="PackingSlipHeading">Shipping Details</div>
+                        <div style={{fontSize: "1.5rem"}} className="PackingSlipHeading">Shipping Details</div>
                         {shippingAdress.map((detail: any) => {
                             return <div>
                                 <div>{detail.first_name} {detail.last_name}</div>
@@ -62,7 +62,7 @@ const PackingSlip = ({ order }: PackingSlipProps) => {
                     </div>
                 </div><br /><br/>
 
-                <div style={{fontSize: "2rem"}} className="PackingSlipDetails">
+                <div style={{fontSize: "1.5rem"}} className="PackingSlipDetails">
                     <div className="PackingSlipDetailsLeft">
                         <div className="DetailRow">
                             <div className="Label">Order:</div>
@@ -83,14 +83,14 @@ const PackingSlip = ({ order }: PackingSlipProps) => {
                 </div><br /><br/>
 
                 <div className="PackingSlipComments" style={{/**"%%GLOBAL_HideComments%%" */}}>
-                    <div style={{fontSize: "2rem"}} className="PackingSlipHeading">Comments:</div>
-                    <blockquote style={{fontSize: "2rem"}}>
+                    <div style={{fontSize: "1.5rem"}} className="PackingSlipHeading">Comments:</div>
+                    <blockquote style={{fontSize: "1.5rem"}}>
                         Ajouter présentoir + commande vrac
                     </blockquote>
                 </div><br/><br/>
 
                 <div className="PackingSlipItems">
-                    <div style={{fontSize: "2rem", marginTop: "3%"}} className="PackingSlipHeading">Shipped Items</div><br /><br/>
+                    <div style={{fontSize: "1.5rem", marginTop: "3%"}} className="PackingSlipHeading">Shipped Items</div><br /><br/>
                     <table style={{
                         width: '100vw',
                         height: 'fit-content'
@@ -98,19 +98,19 @@ const PackingSlip = ({ order }: PackingSlipProps) => {
                         <thead style={{border: '1px solid black', borderBottom: "none"}}>
                             <tr style={{paddingTop: "30px", paddingBottom: "30px"}}>
                                 <th style={{width: "20%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center"}}></th>
-                                <th style={{width: "30%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2rem"}}>Code / SKU</th>
-                                <th style={{width: "10%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2rem"}}>Qty</th>
-                                <th style={{width: "30%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "2rem"}}>Bin Picking No.</th>
+                                <th style={{width: "30%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "1.5rem"}}>Code / SKU</th>
+                                <th style={{width: "10%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "1.5rem"}}>Qty</th>
+                                <th style={{width: "30%", borderRight: '1px solid black', paddingTop: "1%", paddingBottom: "1%", textAlign: "center", fontSize: "1.5rem"}}>Bin Picking No.</th>
                                 <th style={{width: "10%"}}></th>
                             </tr>
                         </thead>
                         <tbody style={{border: '1px solid black'}}>
                             {products && products.map((product: any, index: number) => (
                                 <tr style={{borderBottom: "1px solid black", borderTop: index === (products - 1) && "1px solid black"}}>
-                                    <td style={{textAlign: "center", borderRight: "1px solid black", borderLeft: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}><img src={product.data.image_url} style={{width: "168px", height: "196px"}} /></td>
-                                    <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}>{product.data.sku}</td>
-                                    <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}>12</td>
-                                    <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "2rem"}}>hfxfgx5641654569415</td>
+                                    <td style={{textAlign: "center", borderRight: "1px solid black", borderLeft: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "1.5rem"}}><img src={product.data.image_url} style={{width: "168px", height: "196px"}} /></td>
+                                    <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "1.5rem"}}>{product.data.sku}</td>
+                                    <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "1.5rem"}}>12</td>
+                                    <td style={{textAlign: "center", borderRight: "1px solid black", paddingTop: "1%", paddingBottom: "1%", fontSize: "1.5rem"}}>hfxfgx5641654569415</td>
                                     <td></td>
                                 </tr>
                             ))}
